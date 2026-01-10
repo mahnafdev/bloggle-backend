6332f496e180cf1488e62b77fbeaa5d6a370c703
mahnafdev <mahnafdev@gmail.com>
2026-01-10 12:37:53 +0600

    feat(auth): enable email-password auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,11 +4,18 @@ import { prisma } from "./prisma.ts";
 
 //* Better-Auth Initialization
 const auth = betterAuth({
+	// App and Basic Settings
 	appName: "Bloggle",
 	basePath: "/api/v1/auth",
+	// Database Settings
 	database: prismaAdapter(prisma, {
 		provider: "postgresql",
 	}),
+	// Auth Settings
+	emailAndPassword: {
+		enabled: true,
+		minPasswordLength: 6,
+	},
 });
 
 export { auth };
