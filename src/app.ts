a37c7aa3034167b8704c2814e9818c41a99e7bac
mahnafdev <mahnafdev@gmail.com>
2026-01-11 14:45:03 +0600

    fix(auth): add cors origin and add auth trusted origins

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application } from "express";
 import { postsRouter } from "./modules/posts/posts.router.ts";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.ts";
-
+import cors from "cors";
 //* Express App
 const app: Application = express();
 
@@ -11,6 +11,12 @@ app.all("/api/v1/auth/*splat", toNodeHandler(auth));
 
 //* Global Middlewares
 app.use(express.json());
+app.use(
+	cors({
+		origin: process.env.APP_URL || "http://localhost:3000",
+		credentials: true,
+	}),
+);
 
 //* Modules
 app.use("/api/v1/posts", postsRouter);
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { betterAuth } from "better-auth";
+import { betterAuth, env } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma.ts";
 
@@ -7,6 +7,7 @@ const auth = betterAuth({
 	// App and Basic Settings
 	appName: "Bloggle",
 	basePath: "/api/v1/auth",
+	trustedOrigins: [process.env.APP_URL!],
 	// Database Settings
 	database: prismaAdapter(prisma, {
 		provider: "postgresql",
@@ -14,7 +15,9 @@ const auth = betterAuth({
 	// Auth Settings
 	emailAndPassword: {
 		enabled: true,
+		autoSignIn: false,
 		minPasswordLength: 6,
+		requireEmailVerification: true,
 	},
 });
 
